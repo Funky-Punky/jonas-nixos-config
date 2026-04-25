89d46a2d1fd7c913240765388e11a0d72cce76ff
Honas <jonas.reinstaedtler@web.de>
2026-04-26 01:05:36 +0200

    next iteration haha

--- shared/hyprland/default.nix
+++ shared/hyprland/default.nix
@@ -11,6 +11,10 @@
   #  xwayland.enable = true;
   #};
 
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
 
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
