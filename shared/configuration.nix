16ba2ca69fbdd8c8cca7e7389876c43b0e549900
Honas <jonas.reinstaedtler@web.de>
2026-05-07 14:26:28 +0200

    new programs

--- shared/configuration.nix
+++ shared/configuration.nix
@@ -18,7 +18,6 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    abc-verifier
     texliveFull
     brightnessctl
     light
